464c8614bdad4c06acc772e0c09d3f1220b7ea6c
MHalilov <mhalilov@mrtexpert.ru>
2025-11-07 14:01:47 +0300

    delete all

--- docker/postgres/init.sql
+++
@@ -1,9 +0,0 @@
--- PostgreSQL initialization script for Timelith
--- This file creates necessary extensions and initial settings
-
--- Enable required extensions
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE EXTENSION IF NOT EXISTS "pgcrypto";
-
--- Grant necessary privileges
-GRANT ALL PRIVILEGES ON DATABASE timelith_production TO timelith;
--- rails-app/app/models/schedule_channel.rb
+++
@@ -1,6 +0,0 @@
-class ScheduleChannel < ApplicationRecord
-  belongs_to :schedule
-  belongs_to :channel
-
-  validates :schedule_id, uniqueness: { scope: :channel_id }
-end
